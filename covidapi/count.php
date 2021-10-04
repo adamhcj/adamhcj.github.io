a4086b59cb16dda361e090371ec959d371095959
piplupOwo <82926705+piplupOwo@users.noreply.github.com>
2021-10-04 19:03:17 +0800

    Add files via upload

---
+++ covidapi/count.php
@@ -0,0 +1,121 @@
+<?php
+//=================================================================
+// DO NOT MODIFY THIS FILE
+//=================================================================
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$statistics = [
+    "North America" => [
+        "USA" => [
+            "total_cases" => 43668680,
+            "total_deaths" => 705293,
+            "total_recovered" => 33112993,
+            "active_cases" => 9850394,
+            "population" => 333389570
+        ],
+        "Canada" => [
+            "total_cases" => 1598800,
+            "total_deaths" => 27620,
+            "total_recovered" => 1526156,
+            "active_cases" => 45024,
+            "population" => 38148931
+        ]
+    ],
+    "South America" => [
+        "Brazil" => [
+            "total_cases" => 21327616,
+            "total_deaths" => 593698,
+            "total_recovered" => 20326408,
+            "active_cases" => 407510,
+            "population" => 214417199
+        ],
+        "Argentina" => [
+            "total_cases" => 5248847,
+            "total_deaths" => 114828,
+            "total_recovered" => 5107912,
+            "active_cases" => 26107,
+            "population" => 45705544
+        ]
+        ,
+        "Colombia" => [
+            "total_cases" => 4948513,
+            "total_deaths" => 126068,
+            "total_recovered" => 4788573,
+            "active_cases" => 33872,
+            "population" => 51549372
+        ]
+    ],
+    "Asia" => [
+        "Indonesia" => [
+            "total_cases" => 4206253,
+            "total_deaths" => 141381,
+            "total_recovered" => 4020801,
+            "active_cases" => 44071,
+            "population" => 277078037
+        ],
+        "Japan" => [
+            "total_cases" => 1689899,
+            "total_deaths" => 17421,
+            "total_recovered" => 1627755,
+            "active_cases" => 44723,
+            "population" => 125999832
+        ]
+        ,
+        "Thailand" => [
+            "total_cases" => 1549285,
+            "total_deaths" => 16143,
+            "total_recovered" => 1408602,
+            "active_cases" => 124540,
+            "population" => 70015456
+        ]
+        ,
+        "Singapore" => [
+            "total_cases" => 84510,
+            "total_deaths" => 73,
+            "total_recovered" => 73395,
+            "active_cases" => 11042,
+            "population" => 5906908
+        ]
+    ],
+    "Europe" => [
+        "France" => [
+            "total_cases" => 6983601,
+            "total_deaths" => 116420,
+            "total_recovered" => 6709622,
+            "active_cases" => 157559,
+            "population" => 65451212
+        ],
+        "Spain" => [
+            "total_cases" => 4946601,
+            "total_deaths" => 86229,
+            "total_recovered" => 4708167,
+            "active_cases" => 152205,
+            "population" => 46777063
+        ],
+        "Italy" => [
+            "total_cases" => 4653696,
+            "total_deaths" => 130603,
+            "total_recovered" => 4419537,
+            "active_cases" => 103556,
+            "population" => 60352399
+        ]
+    ]
+];
+
+$result_arr = array();
+$result_arr["statistics"] = $statistics;
+
+$date = new DateTime(null, new DateTimeZone('Asia/Singapore'));
+$result_arr["info"] = array(
+    "author" => "Covid Live Update Service, Inc.",
+    "response_datetime_singapore" => $date->format('Y-m-d H:i:sP')
+);
+
+// set response code - 200 OK
+http_response_code(200);
+
+// show response
+echo json_encode($result_arr);
+
+?>
