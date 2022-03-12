402f89c300b09802e88b41505d0a676b91ad1df8
piplupOwo <adam334123@gmail.com>
2022-03-12 15:45:26 +0800

    rename v2

--- dbstest/login.js
+++ dbstest/login.js
@@ -1,7 +1,7 @@
 const CLIENTID = 'e4c21e05-7741-4dec-a4e3-9d428f7fa5f2'
 const CLIENTSECRET = 'd419717c-c40a-4796-850f-bb921e4ca3af'
 
-const redirecturi = 'http://piplupowo.github.io/dbstestapi/authenticate'
+const redirecturi = 'http://piplupowo.github.io/dbstest/authenticate'
 
 const authurl = `https://www.dbs.com/sandbox/api/sg/v1/oauth/authorize?client_id=${CLIENTID}&scope=Read&response_type=code&redirect_uri=${redirecturi}&state=0399&login_page=card`
 
