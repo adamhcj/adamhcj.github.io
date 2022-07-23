5e4d60ba646c1743825b8a4f793567a9fabc76a8
Stabilo <zteo015@e.ntu.edu.sg>
2022-07-23 16:18:07 +0800

    re-organised files into relevant folders and formatted js documents with vscode

---
+++ js/fallingobjects.js
@@ -0,0 +1,60 @@
+function random(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function dropBox() {
+  var length = random(50, $(".game").width() - 50);
+  var velocity = random(10000, 50000);
+  var size = random(50, 150);
+  var thisBox = $("<div/>", {
+    class: "box",
+    style:
+      "width:" +
+      size +
+      "px; height:" +
+      size +
+      "px; left:" +
+      length +
+      "px; transition: transform " +
+      velocity +
+      "ms linear;",
+  });
+
+  //set data and bg based on data
+  thisBox.data("test", Math.round(Math.random()));
+  if (thisBox.data("test")) {
+    thisBox.css({
+      background:
+        "url('https://cdn.discordapp.com/attachments/818102115058450474/887315665273368596/pipbw.gif')",
+      "background-size": "contain",
+      "background-repeat": "no-repeat",
+    });
+  } else {
+    thisBox.css({
+      background:
+        "url('https://cdn.discordapp.com/attachments/818102115058450474/887315665273368596/pipbw.gif')",
+      "background-size": "contain",
+      "background-repeat": "no-repeat",
+    });
+  }
+
+  //insert gift element
+  $(".game").append(thisBox);
+
+  //random start for animation
+  setTimeout(function () {
+    thisBox.addClass("move");
+  }, random(0, 20));
+
+  //remove this object when animation is over
+  thisBox.one(
+    "webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
+    function (event) {
+      $(this).remove();
+    }
+  );
+}
+
+setInterval(() => {
+  dropBox();
+}, 2000);
